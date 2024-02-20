62139d92c4ebda282bde7deccfba16379d6eb0aa
ELENAMARIAVARGHESE <elenamariavarghese@gmail.com>
2024-02-20 12:31:46 +0530

    first commit

---
+++ src/component/Button.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { TouchableOpacity,View,Text,StyleSheet } from 'react-native'
+
+const ButtonComponent = () => {
+  
+
+    return (
+        <View >
+                  <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+        </View>
+    );};
+
+    const styles = StyleSheet.create({
+        
+        button: {
+          alignItems: 'center',
+          backgroundColor: '#181818',
+          borderRadius:10,
+          width:345,
+          padding: 10,
+          marginTop: 20,
+        },
+        buttonText: {
+          color: '#ffffff',
+        },
+        
+      });
+      export default ButtonComponent;
